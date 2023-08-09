e3438348839484c22ca196d1917a65c611d2be50
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-08-09 13:07:14 +0300

    add

--- src/components/Statics.js
+++ src/components/Statics.js
@@ -1,11 +1,13 @@
 import { Component } from "react";
+import '../components/Statics.css';
 
 export class Statics extends Component {
 
-state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    state = {
+        visible: false,
+        good: 0,
+        neutral: 0,
+        bad: 0,
     }
     
     // plusGood = () => {
@@ -26,9 +28,16 @@ state = {
     //     }))
     // }
 
+    // toggle = () => {
+    //     this.setState(prevState => ({
+    //         visible: !prevState.visible
+    //     }))
+    // }
+
     plusActivity = (activity) => {
         this.setState(prevState => ({
-            [`${activity}`]: prevState[`${activity}`] + 1,
+            visible: true,
+            [activity]: prevState[activity] + 1,
     }))
     }
 
@@ -41,18 +50,36 @@ state = {
             <>
                 <h1>Please leave feeatback</h1>
 
-                <button onClick={()=> this.plusActivity('good')}>Good</button>
-                <button onClick={()=> this.plusActivity('neutral')}>Neutral</button>
-                <button onClick={() => this.plusActivity('bad')}>Bad</button>
+                <div className="button">
+                    <button className="buttonGood"
+                        onClick={() =>this.plusActivity('good')}>
+                        Good
+                    </button>
+                    <button className="buttonNeutral"
+                        onClick={() => this.plusActivity('neutral')}>
+                        Neutral
+                    </button>
+                    <button className="buttonBad"
+                        onClick={() => this.plusActivity('bad')}>
+                        Bad
+                    </button>
+                </div>
                 
                 <h2>Statics</h2>
-                <ul>
-                    <li>Good: {good}</li>
-                    <li>Neutral: {neutral}</li>
-                    <li>Bad: {bad}</li>
-                    <li>Total: {total} </li>
-                    <li>Positive feeadback: {positivePercentage.toFixed(0)}%</li>
-                </ul>
+
+                {this.state.visible ?
+                    
+                    <ul className="statics">
+                        <li>Good: <span>{good}</span></li>
+                        <li>Neutral: <span>{neutral}</span></li>
+                        <li>Bad: <span>{bad}</span></li>
+                        <li>Total: <span>{total}</span></li>
+                        <li className="statics-positive">Positive feeadback:
+                            <span>{positivePercentage.toFixed(0)}%</span>
+                        </li>
+                    </ul>
+
+                : <h3 className="No-feedback">No feedback given</h3>}
             </>
         )
     }
