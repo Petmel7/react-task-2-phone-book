5d9f85747d661a51c3f6696ede7b1e07c12ce174
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-08-31 11:31:58 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+
+import React, { useState, useEffect } from "react";
 import shortid from "shortid";
 import ContactForm from "./components/ContactForm.js";
 import ContactList from "./components/ContactList.js";
@@ -6,84 +7,69 @@ import './components/Contacts.css';
 import './App.css';
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
-  const [name, setName] = useState('')
-  const [number, setNumber] = useState('')
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
   useEffect(() => {
-  const contacts = localStorage.getItem('contacts');
-  const parsedContacts = JSON.parse(contacts);
+    const storedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (storedContacts) {
+      setContacts(storedContacts);
+    }
+  }, []);
 
-  if (parsedContacts) {
-    setContacts(parsedContacts);
-  }
-}, []);
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-const submitPhoneBook = event => {
-  event.preventDefault();
+  const submitPhoneBook = event => {
+    event.preventDefault();
 
-  const existingContact = contacts.find(contact => contact.name === name);
-  if (existingContact) {
-    alert(`This name ${name} already exists`);
-    return;
-  }
+    const existingContact = contacts.find(contact => contact.name === name);
+    if (existingContact) {
+      alert(`This name ${name} already exists`);
+      return;
+    }
 
-  const contact = { 
-    id: shortid.generate(),
-    name: name,
-    number: number,
-    completed: false
+    const contact = { 
+      id: shortid.generate(),
+      name: name,
+      number: number,
+      completed: false
+    };
+
+    setContacts(prevContacts => [...prevContacts, contact]);
+    setName('');
+    setNumber('');
   };
 
-  setContacts([...contacts, contact]);
-  setName('');
-  setNumber('');
-
-  // Очищаємо localStorage від старих даних
-  localStorage.setItem('contacts', JSON.stringify([...contacts, contact]));
-};
-
-
   const foneBookChange = event => {
-    setName(event.currentTarget.value)
-    ///
+    const { name, value } = event.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "number") {
+      setNumber(value);
+    }
   };
 
   const filterChange = event => {
     setFilter(event.currentTarget.value);
   };
 
-  // const getVisibleСontacts = () => {
-  //   const normalizeFilter = filter.toLowerCase();
-
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizeFilter)
-  //   );
-  // }
-
-const getVisibleСontacts = () => {
-  if (contacts) {
+  const getVisibleСontacts = () => {
     const normalizeFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizeFilter)
     );
-  } else {
-    return [];
-  }
-};
+  };
 
   const DeleteContacts = contactId => {
-  setContacts(prevContacts =>
-    prevContacts.filter(contact => contact.id !== contactId)
-  );
-};
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
 
   const visibleСontactsFilter = getVisibleСontacts();
 
